81fd854ae3eb45d968cd50463492ecc2a4adc185
Hukkenberg <masterhieuvd2002@outlook.com>
2024-11-23 10:19:32 +0700

    update #3

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Thay đường dẫn nếu cần
+  baseURL: "https://thesis24.onrender.com", // Thay đường dẫn nếu cần
   headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 });
 
