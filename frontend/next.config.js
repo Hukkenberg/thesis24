604e75017868907025fa2839d0321c2a330e105c
Hukkenberg <masterhieuvd2002@outlook.com>
2024-11-23 15:34:19 +0700

    update #12

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
 };
+
+module.exports = nextConfig;
