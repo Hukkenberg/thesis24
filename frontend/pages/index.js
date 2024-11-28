fa3b581beadc8946d961db43184fe9e72da7d824
Hukkenberg <masterhieuvd2002@outlook.com>
2024-11-28 10:16:39 +0700

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,10 @@
-...
+const Home = ({ role }) => {
+    return (
+      <div>
+        <h1>Welcome to the {role.charAt(0).toUpperCase() + role.slice(1)} Dashboard</h1>
+        <p>This is your customized dashboard.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
