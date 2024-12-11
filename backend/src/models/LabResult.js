cb1f564170c61defa7c09cecfeccf762ddc6aba7
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2024-12-11 23:30:44 +0700

    Update LabResult.js

--- backend/src/models/LabResult.js
+++ backend/src/models/LabResult.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { sequelize } = require('../config/db');
 
 const LabResult = sequelize.define('LabResult', {
     id: {
