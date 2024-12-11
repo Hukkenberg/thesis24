8ae6c7a565a92ec0a0c9a24c72af1d40fe28506d
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2024-12-11 23:30:09 +0700

    Update Appointment.js

--- backend/src/models/Appointment.js
+++ backend/src/models/Appointment.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database'); // Assuming this is your Sequelize instance.
+const { sequelize } = require('../config/db');
 
 const Appointment = sequelize.define('Appointment', {
     id: {
