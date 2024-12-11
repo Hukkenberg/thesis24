e94ac40afb424c4e32165de96b9c0359f29850c6
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2024-12-11 23:07:29 +0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const patientRoutes = require('./routes/patientRoutes');
-const doctorRoutes = require('./routes/doctorRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const labRoutes = require('./routes/labRoutes');
-const appointmentRoutes = require('./routes/appointmentRoutes');
+const routes = require('./routes');
+const middlewares = require('./middlewares');
+const dbConnect = require('./config/db');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use('/auth', authRoutes);
-app.use('/patients', patientRoutes);
-app.use('/doctors', doctorRoutes);
-app.use('/admins', adminRoutes);
-app.use('/labs', labRoutes);
-app.use('/api/appointments', appointmentRoutes);
+dbConnect();
+
+app.use(express.json());
+app.use(middlewares);
+app.use('/api', routes);
 
 module.exports = app;
