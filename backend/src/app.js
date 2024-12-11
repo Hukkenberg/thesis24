5abd980164e75ceb45b2a9c19de537c73b1251c3
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2024-12-12 00:18:12 +0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
-const middlewares = require('./middlewares');
 const { dbConnect } = require('./config/db');
+const { authenticate } = require('./middlewares/authMiddleware');
+const { authorize } = require('./middlewares/roleMiddleware');
 
 const app = express();
 
 dbConnect();
 
 app.use(express.json());
-app.use(middlewares);
+// Use middleware explicitly if needed globally
+app.use(authenticate);
 app.use('/api', routes);
 
 module.exports = app;
