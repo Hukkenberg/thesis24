a28573662fb0b7ac867612e4de3af753a0b4c0ad
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2024-12-11 23:21:40 +0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const { PORT } = require('./config/environment');
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
