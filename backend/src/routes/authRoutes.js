13302b26d5338e5aca1f482203aed65320005d73
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2024-12-11 15:14:51 +0700

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { login, refreshToken } = require('../controllers/authController');
 const { validateLogin } = require('../validators/authValidator');
+const { verifyToken } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/login', validateLogin, login);
-router.post('/refresh', refreshToken);
+router.post('/refresh', verifyToken, refreshToken);
 
 module.exports = router;
