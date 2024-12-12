db5fd66f8e07b9c1ad50b565030cd10514f977c1
Hukkenberg <masterhieuvd2002@outlook.com>
2024-12-12 11:24:06 +0700

    Update 001_seed_users.sql

--- database/seeds/001_seed_users.sql
+++ database/seeds/001_seed_users.sql
@@ -1,6 +1,14 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'pgcrypto') THEN
+        CREATE EXTENSION pgcrypto;
+    END IF;
+END
+$$;
+
 INSERT INTO users (name, email, password, role, created_at, updated_at)
 VALUES
-  ('Admin User', 'admin@example.com', crypt('admin_password', gen_salt('bf')), 'admin', NOW(), NOW()),
-  ('Doctor User', 'doctor@example.com', crypt('doctor_password', gen_salt('bf')), 'doctor', NOW(), NOW()),
-  ('Lab User', 'lab@example.com', crypt('lab_password', gen_salt('bf')), 'lab', NOW(), NOW()),
-  ('Patient User', 'patient@example.com', crypt('patient_password', gen_salt('bf')), 'patient', NOW(), NOW());
+    ('Admin User', 'admin@example.com', crypt('admin_password', gen_salt('bf')), 'admin', NOW(), NOW()),
+    ('Doctor User', 'doctor@example.com', crypt('doctor_password', gen_salt('bf')), 'doctor', NOW(), NOW()),
+    ('Lab User', 'lab@example.com', crypt('lab_password', gen_salt('bf')), 'lab', NOW(), NOW()),
+    ('Patient User', 'patient@example.com', crypt('patient_password', gen_salt('bf')), 'patient', NOW(), NOW());
