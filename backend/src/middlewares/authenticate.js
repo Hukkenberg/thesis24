edaf948905e665d3802447cabbc8911998f3851b
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2024-12-13 02:44:12 +0700

    Update authenticate.js

--- backend/src/middlewares/authenticate.js
+++ backend/src/middlewares/authenticate.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 exports.authenticate = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
   if (!token) {
-    return res.status(401).json({ error: 'No token provided' });
+    return next(); // Không bắt buộc xác thực cho tất cả các route
   }
 
   try {
@@ -15,4 +15,3 @@ exports.authenticate = (req, res, next) => {
     return res.status(401).json({ error: 'Invalid token' });
   }
 };
-
