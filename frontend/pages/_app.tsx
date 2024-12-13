e7f2e8f10cb82ca192659368bbb2f9edcf28b055
Hukkenberg <78076365+Hukkenberg@users.noreply.github.com>
2024-12-13 08:52:40 +0700

    Update _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { createContext, useContext } from 'react';
 import NavigationBar from '../components/NavigationBar';
+import { createContext, useContext } from 'react';
 
 const AuthContext = createContext({ role: 'patient' });
 
