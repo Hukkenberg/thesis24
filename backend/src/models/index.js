42992e4ccd382ffd0ea03aab41434f7ce2f00cb8
Hukkenberg <masterhieuvd2002@outlook.com>
2024-12-14 18:15:56 +0700

    Create index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,16 @@
+
+const User = require('./User');
+const Doctor = require('./Doctor');
+const Patient = require('./Patient');
+const Appointment = require('./Appointment');
+const LabResult = require('./LabResult');
+const Prescription = require('./Prescription');
+
+module.exports = {
+  User,
+  Doctor,
+  Patient,
+  Appointment,
+  LabResult,
+  Prescription,
+};
