8dce212ce5504c90aa19d35b12046ad12562883b
Hukkenberg <masterhieuvd2002@outlook.com>
2024-12-14 13:51:33 +0700

    Create Admin.js

---
+++ backend/src/models/Admin.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database'); // Adjust the path to your database configuration
+
+const Admin = sequelize.define('Admin', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    timestamps: true,
+    tableName: 'Admins',
+});
+
+module.exports = Admin;
