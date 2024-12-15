8e9882a478efa2ee73489fc5f90edd5c4749ef10
Hukkenberg <masterhieuvd2002@outlook.com>
2024-12-15 14:06:44 +0700

    final #1

--- frontend/components/esm-active-visits-app/active-visits.component.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function ActiveVisits() {
-  return (
-    <div>
-      <h1>Active Visits</h1>
-      <p>Manage active visits and tests here.</p>
-    </div>
-  );
-}
--- frontend/components/esm-patient-list-management-app/dashboard.meta.ts
+++
@@ -1,5 +0,0 @@
-export const dashboardMeta = {
-  name: 'patient-lists',
-  slot: 'patient-lists-dashboard-slot',
-  title: 'Patient lists',
-};
