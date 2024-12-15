220e04245281b76e5ed55d12dd15746cea0a68e7
Hukkenberg <masterhieuvd2002@outlook.com>
2024-12-15 13:03:34 +0700

    Update tests.js

--- frontend/pages/tests.js
+++ frontend/pages/tests.js
@@ -1,11 +1,11 @@
 
-import ActiveTests from '../components/esm-active-visits-app/root.component';
+import ActiveVisits from '../components/esm-active-visits-app/active-visits.component';
 
 export default function TestsPage() {
   return (
     <div>
       <h1>Test Management</h1>
-      <ActiveTests />
+      <ActiveVisits />
     </div>
   );
 }
