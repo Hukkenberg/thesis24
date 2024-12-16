6407191f5b9232701bea1dd0948293cfee9360d7
Hukkenberg <masterhieuvd2002@outlook.com>
2024-12-16 12:13:26 +0700

    final #35

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 
 const express = require('express');
+const cors = require('cors'); // Import CORS
 const app = express();
 const authRoutes = require('./routes/auth');
 
+// CORS Configuration
+const corsOptions = {
+  origin: ['https://thesis24-web.onrender.com'], // Allow frontend domain
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions)); // Apply CORS
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
